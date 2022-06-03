c6472b2fd9f4a892f106633b8b4369f325bb834c
wed <wedarobi@pm.me>
2022-06-03 15:03:56 +1000

    dllMgr: Loop over all DLLs

--- manager/main.js
+++ manager/main.js
@@ -1105,43 +1105,55 @@ async function main()
 
     // log(`> Compiling...`);
 
-    let results = [];
+    let results1 = [];
 
-    for (let romVer of ["usa", "jpn", "eur", "aus"])
+    for (let dllName of
+    [
+        "chmrtannoy",
+        "cosection",
+    ])
     {
-        update_rom_version(romVer);
+        let results2 = [];
 
-        // cosection, chmrtannoy
-        let dllName = "chmrtannoy";
+        for (let romVer of ["usa", "jpn", "eur", "aus"])
+        {
+            update_rom_version(romVer);
+    
+            // cosection, chmrtannoy
+            // let dllName = "chmrtannoy";
+    
+            let fn_o = await dll_build(dllName);
+    
+            let [fn_raw, file_raw] = await dll_process(dllName, fn_o);
+    
+            await dll_preheader_encrypt(fn_raw, file_raw);
+    
+            let similarity = await get_similarity_dll(gSyscallIdxMap[dllName], file_raw);
+    
+            // console.log
+            results2.push
+            (
+                similarity.found && similarity.similarity === 1
+                ? gct(`[${gRomVer}] >>> MATCH!!!`.padEnd(18, " "), "green")
+                : gct(`[${gRomVer}] (bad: ${(similarity.similarity * 100).toFixed(1)}%)`.padEnd(18, " "), "red")
+            );
+    
+    
+    
+            // await dll_package(fn_raw, file_raw);
+    
+    
+    
+            // await dll_package("/mnt/r/chcoderoombits_edited.bin")
+    
+    
+        }
 
-        let fn_o = await dll_build(dllName);
-
-        let [fn_raw, file_raw] = await dll_process(dllName, fn_o);
-
-        await dll_preheader_encrypt(fn_raw, file_raw);
-
-        let similarity = await get_similarity_dll(gSyscallIdxMap[dllName], file_raw);
-
-        // console.log
-        results.push
-        (
-            similarity.found && similarity.similarity === 1
-            ? gct(`[${gRomVer}] >>> MATCH!!`, "green")
-            : gct(`[${gRomVer}] (differs: ${(similarity.similarity * 100).toFixed(1)}%)`, "red")
-        );
-
-
-
-        // await dll_package(fn_raw, file_raw);
-
-
-
-        // await dll_package("/mnt/r/chcoderoombits_edited.bin")
-
-
+        results1.push(gct(`[${dllName}] `.padEnd(25, " "), "cyan") + results2.join(" "));
     }
 
-    console.log(results.join("\n"))
+
+    console.log(results1.join("\n"))
 
 
     log(`Done!`);
